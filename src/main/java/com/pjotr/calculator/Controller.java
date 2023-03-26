556dde18dc2e30b47a5ddf2d59b039f3ff0fa6ca
Peter-The-Great <pjotrw15@gmail.com>
2023-03-26 21:35:35 +0200

    Verandering van de applicatie
    Een gehele nieuwe verandering van de applicatie om een echte wiskundige rekenmachine te maken.

---
+++ src/main/java/com/pjotr/calculator/Controller.java
@@ -0,0 +1,323 @@
+package com.pjotr.calculator;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.stage.Popup;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    static CheckMenuItem select_menu = new CheckMenuItem();
+    ArrayList<String> arrayList = new ArrayList<>();
+    String expression, number=" ";
+    char type = 'D';
+    @FXML
+    Label answer, history;
+    @FXML
+    Button sin, tan, cos, fact, pi, ln, Inverse, e, log, equal;
+    @FXML
+    RadioButton Scientific, Standard, Degree, Radian;
+    @FXML
+    TextField box;
+    int x = 0;
+    @FXML
+    CheckMenuItem len;
+    @FXML
+    RadioMenuItem converter_item,scientific_item,standard_item;
+    @FXML
+    public void numbers(ActionEvent actionEvent) {
+        Button button = (Button) actionEvent.getSource();
+        if (Character.isDigit(button.getText().charAt(0))||button.getText().equals("e")||button.getText().equals("π")) {
+            number = number + button.getText();
+            box.setText(box.getText() + button.getText());
+            arrayList.add(button.getText());
+        }
+        if (button.getText().equals(".") && !number.contains(".")) {
+            number = number + button.getText();
+            box.setText(box.getText() + button.getText());
+            arrayList.add(button.getText());
+        }
+        expression = box.getText();
+    }
+    @FXML
+    protected void power(){
+        if (!number.equals(" ")){
+            box.setText(box.getText()+"^(");
+            arrayList.add("^(");
+            expression=box.getText();
+        }
+
+    }
+    @FXML
+    protected void Operator(ActionEvent actionEvent) {
+        Button button = (Button) actionEvent.getSource();
+        if (!box.getText().equals(""))
+        {
+            if (!number.equals(" ")) {
+                box.setText(expression + button.getText());
+                arrayList.add(button.getText());
+                x = arrayList.size() - 1;
+            }
+            else if (!box.getText().equals("-")){
+                box.setText(expression + button.getText());
+                arrayList.set(x, button.getText());
+            }
+        }
+        if (button.getText().equals("-") && box.getText().equals("")) {
+            arrayList.add(button.getText());
+            box.setText(box.getText() + button.getText());
+            expression = box.getText();
+        }
+        System.out.println(arrayList);
+        number = " ";
+    }
+
+    @FXML
+    protected void brackets(ActionEvent actionEvent) {
+        Button button = (Button) actionEvent.getSource();
+        arrayList.add(button.getText());
+        box.setText(box.getText() + button.getText());
+        expression = box.getText();
+        number = " ";
+    }
+
+    @FXML
+    protected void Evaluate() {
+        if (!box.getText().equals("")) {
+            try {
+                Calculator calculator = new Calculator(box.getText(), type);
+                history.setText(calculator.mem + "=");
+                answer.setText("" + calculator.findAnswers());
+            } catch (Exception e) {
+                box.setText("");
+                answer.setText("Bad Expression");
+            }
+        }
+    }
+
+    @FXML
+    protected void clear() {
+        expression = "";
+        answer.setText("");
+        arrayList.clear();
+        history.setText("");
+        box.setText(expression);
+        number = " ";
+    }
+
+    @FXML
+    protected void MathFunction(ActionEvent actionEvent) {
+        Button button = (Button) actionEvent.getSource();
+        if (button.getText().equals("n!")) {
+            arrayList.add("!");
+            box.setText(box.getText() + "!");
+            expression = box.getText();
+        }
+        if (button.getText().equals("sin")) {
+            arrayList.add("sin(");
+            box.setText(box.getText() + "sin(");
+            expression = box.getText();
+        }
+        if (button.getText().equals("asin")) {
+            arrayList.add("asin(");
+            box.setText(box.getText() + "asin(");
+            expression = box.getText();
+        }
+        if (button.getText().equals("cos")) {
+            arrayList.add("cos(");
+            box.setText(box.getText() + "cos(");
+            expression = box.getText();
+        }
+        if (button.getText().equals("acos")) {
+            arrayList.add("acos(");
+            box.setText(box.getText() + "acos(");
+            expression = box.getText();
+        }
+        if (button.getText().equals("tan")) {
+            arrayList.add("tan(");
+            box.setText(box.getText() + "tan(");
+            expression = box.getText();
+        }
+        if (button.getText().equals("atan")) {
+            arrayList.add("atan(");
+            box.setText(box.getText() + "atan(");
+            expression = box.getText();
+        }
+        if (button.getText().equals("log")) {
+            arrayList.add("log(");
+            box.setText(box.getText() + "log(");
+            expression = box.getText();
+        }
+        if (button.getText().equals("10^x")) {
+            arrayList.add("10^");
+            box.setText(box.getText() + "10^");
+            expression = box.getText();
+        }
+        if (button.getText().equals("ln")) {
+            arrayList.add("ln(");
+            box.setText(box.getText() + "ln(");
+            expression = box.getText();
+        }
+        if (button.getText().equals("e^x")) {
+            arrayList.add("e^");
+            box.setText(box.getText() + "e^");
+            expression = box.getText();
+        }
+        if (button.getText().equals("√")) {
+            arrayList.add("√(");
+            box.setText(box.getText() + "√(");
+            expression = box.getText();
+        }
+        number = " ";
+    }
+    @FXML
+    public void rational(){
+        if (!answer.getText().isEmpty()){
+            Rational rational=new Rational(answer.getText());
+            answer.setText(rational.P_by_QForm());
+        }
+    }
+     @FXML
+     public void showPopUp(){
+         Popup popup=new Popup();
+         popup.setOpacity(0.80);
+         popup.setX(Main.stage.getX()+70);
+         popup.setY(Main.stage.getY()+80);
+         popup.getContent().add(Main.pop);
+         popup.setAutoHide(true);
+         if (!popup.isShowing()){
+             popup.show(Main.stage);
+         }
+     }
+    @FXML
+    protected void cut() {
+        if (arrayList.size() != 0) {
+            expression = "";
+            arrayList.remove(arrayList.size() - 1);
+            for (String s : arrayList) expression = String.format("%s%s", expression, s);
+            box.setText(expression);
+        }
+    }
+
+    @FXML
+    protected void InverseFunction(ActionEvent actionEvent) {
+        Button button = (Button) actionEvent.getSource();
+        if (!button.isDefaultButton()) {
+            log.setText("10^x");
+            ln.setText("e^x");
+            sin.setText("asin");
+            cos.setText("acos");
+            tan.setText("atan");
+            button.setDefaultButton(true);
+            button.setStyle("-fx-background-color:#0bade0;-fx-text-fill:white");
+        } else {
+            button.setDefaultButton(false);
+            button.setStyle("-fx-background-color:white");
+            log.setText("log");
+            ln.setText("ln");
+            sin.setText("sin");
+            cos.setText("cos");
+            tan.setText("tan");
+        }
+    }
+
+    @FXML
+    protected void Enable(ActionEvent actionEvent) {
+        number = " ";
+        if (((RadioButton) actionEvent.getSource()).getText().equals("Scientific")) {
+            Standard.setSelected(false);
+            standard_item.setSelected(false);
+            scientific_item.setSelected(true);
+            Select();
+        }
+        if (((RadioButton) actionEvent.getSource()).getText().equals("Standard")) {
+            Scientific.setSelected(false);
+            scientific_item.setSelected(false);
+            standard_item.setSelected(true);
+            DeSelect();
+        }
+
+    }
+    void Select(){
+        sin.setDisable(false);
+        cos.setDisable(false);
+        tan.setDisable(false);
+        fact.setDisable(false);
+        pi.setDisable(false);
+        ln.setDisable(false);
+        log.setDisable(false);
+        Inverse.setDisable(false);
+        e.setDisable(false);
+    }
+    void DeSelect(){
+        sin.setDisable(true);
+        cos.setDisable(true);
+        tan.setDisable(true);
+        fact.setDisable(true);
+        pi.setDisable(true);
+        ln.setDisable(true);
+        log.setDisable(true);
+        Inverse.setDisable(true);
+        e.setDisable(true);
+    }
+    @FXML
+    protected void measureSystem(ActionEvent actionEvent) {
+        if (((RadioButton) actionEvent.getSource()).getText().equals("Degree")) {
+            Radian.setSelected(false);
+            type = 'D';
+        }
+        if (((RadioButton) actionEvent.getSource()).getText().equals("Radian")) {
+            type = 'R';
+            Degree.setSelected(false);
+        }
+    }
+
+    @FXML
+    protected void Menu_items(ActionEvent actionEvent) {
+        RadioMenuItem radioMenuItem = (RadioMenuItem) actionEvent.getSource();
+        if (radioMenuItem.getText().equals("Conversion")) {
+            if(radioMenuItem.isSelected())
+            Main.stage.setWidth(985 + 65);
+            else Main.stage.setWidth(545 + 15);
+        }
+        if (radioMenuItem.getText().equals("Scientific"))
+        {
+            if(radioMenuItem.isSelected())
+            {
+                standard_item.setSelected(false);
+                Standard.setSelected(false);
+                Scientific.setSelected(true);
+                Select();
+            }
+            else {
+                standard_item.setSelected(true);
+                Standard.setSelected(true);
+                Scientific.setSelected(false);
+                DeSelect();
+            }
+        }
+        if (radioMenuItem.getText().equals("Standard")){
+            if(radioMenuItem.isSelected())
+            {
+                Scientific.setSelected(false);
+                Standard.setSelected(true);
+                scientific_item.setSelected(false);
+                DeSelect();
+            }
+            else {
+                scientific_item.setSelected(true);
+                Standard.setSelected(false);
+                Scientific.setSelected(true);
+                Select();
+            }
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        select_menu = len;
+    }
+}
---
+++ src/main/java/com/pjotr/calculator/Main.java
@@ -0,0 +1,54 @@
+package com.pjotr.calculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+    // Hier worden de variabelen gedeclareerd.
+    // Deze variabelen zijn static, zodat ze in andere klassen kunnen worden aangeroepen.
+
+    // De eerste is een stage, deze wordt gebruikt om de applicatie te tonen.
+    static Stage stage;
+
+    //Dit is een AnchorPane, dit is een paneel waarin andere elementen kunnen worden geplaatst.
+    static AnchorPane parent, pop;
+
+    //Dit is een scene, deze wordt gebruikt om de AnchorPane te tonen.
+    static Scene scene;
+
+    /**
+     * @start
+     * @param primaryStage
+     * Deze methode start de javaFX library en laad de twee fxml bestanden in. Als er een fout optreedt, wordt deze afgevangen via een exception.
+     */
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        pop = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("PopUpPane.fxml")));
+        parent = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("calculator_layout.fxml")));
+        scene = new Scene(parent);
+        stage = primaryStage;
+        primaryStage.setScene(scene);
+        primaryStage.setOpacity(0.95);
+        primaryStage.setTitle("Calculator");
+        primaryStage.getIcons().addAll(new Image(Objects.requireNonNull(Main.class.getResource("images/icon.png")).toString()));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    /**
+     * @main
+     * @param args
+     * Deze methode start de applicatie.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
