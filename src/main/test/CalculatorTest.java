efbf3efb44e7366e7c50b0b958d9f7b408ad541a
Peter-The-Great <pjotrw15@gmail.com>
2023-03-31 10:49:03 +0200

    Nog een keer testen

--- src/main/test/CalculatorTest.java
+++ src/main/test/CalculatorTest.java
@@ -9,41 +9,41 @@ public class CalculatorTest {
     public void testAddition() {
         CalculatorOperation calc = new CalculatorOperation();
         calc.Operation("1+2");
-        assertEquals(3, calc.AdditionAndSubtraction(), DELTA);
+        assertEquals(3.0, calc.AdditionAndSubtraction(), DELTA);
     }
 
     @Test
     public void testSubtraction() {
         CalculatorOperation calc = new CalculatorOperation();
         calc.Operation("3-2");
-        assertEquals(1, calc.AdditionAndSubtraction(), DELTA);
+        assertEquals(1.0, calc.AdditionAndSubtraction(), DELTA);
     }
 
     @Test
     public void testMultiplication() {
         CalculatorOperation calc = new CalculatorOperation();
         calc.Operation("3*4");
-        assertEquals(12, calc.multiplication(), DELTA);
+        assertEquals(12.0, calc.multiplication(), DELTA);
     }
 
     @Test
     public void testDivision() {
         CalculatorOperation calc = new CalculatorOperation();
         calc.Operation("10/5");
-        assertEquals(2, calc.division(), DELTA);
+        assertEquals(2.0, calc.division(), DELTA);
     }
 
     @Test
     public void testPower() {
         CalculatorOperation calc = new CalculatorOperation();
         calc.Operation("2^3");
-        assertEquals(8, calc.power(), DELTA);
+        assertEquals(8.0, calc.power(), DELTA);
     }
     @Test
     public void testFactorial() {
         MathUtils mathUtils = new MathUtils();
-        assertEquals(1.0, mathUtils.factorial(0));
-        assertEquals(1.0, mathUtils.factorial(1));
-        assertEquals(120.0, mathUtils.factorial(5));
+        assertEquals(1.0, mathUtils.factorial(0), DELTA);
+        assertEquals(1.0, mathUtils.factorial(1), DELTA);
+        assertEquals(120.0, mathUtils.factorial(5), DELTA);
     }
 }
